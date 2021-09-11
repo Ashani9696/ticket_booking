12b10b627d0f5576abef0c147372b1e93faf6f52
Ashani9696 <85482403+Ashani9696@users.noreply.github.com>
2021-09-11 09:30:50 +0530

    updated

---
+++ ticket_booking/src/ticket_booking/Ticket_booking.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticket_booking;
+
+/**
+ *
+ * @author Boolean
+ */
+public class Ticket_booking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
